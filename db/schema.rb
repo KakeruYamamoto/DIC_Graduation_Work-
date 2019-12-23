d71badc3c0075f98481e512e09a880dc56587ec5
kakeru <okinawan.kkr0713@icloud.com>
2019-12-23 15:11:58 +0900

    スキーマ更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_055946) do
+ActiveRecord::Schema.define(version: 2019_12_23_060450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2019_12_23_055946) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "parthicipants", force: :cascade do |t|
+    t.integer "event_id"
+    t.integer "parthicipant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
