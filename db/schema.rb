c20948221a25b8b47f133e63351a4abd4aa25b62
kakeru <okinawan.kkr0713@icloud.com>
2019-12-23 15:20:58 +0900

    contactモデル作成 & スキーマ更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_060450) do
+ActiveRecord::Schema.define(version: 2019_12_23_061849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "email", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title", null: false
