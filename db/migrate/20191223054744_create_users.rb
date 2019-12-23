2317b4e584803dfc31c8a825b6f8d1088d1ae21f
kakeru <okinawan.kkr0713@icloud.com>
2019-12-23 14:57:06 +0900

    スキーマ更新

--- db/migrate/20191223054744_create_users.rb
+++ db/migrate/20191223054744_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.integer :id
       t.string :name
       t.string :email
       t.string :passord
       t.string :icon
-      t.boolean　 :admin
+      t.boolean :admin, default: false
+      t.index :email, unique: true
 
       t.timestamps
     end
